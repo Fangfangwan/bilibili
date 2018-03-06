1271b5b919b2ea0871d66cd3aee0b6be1fc959ff
yilundai <32254395+yilundai@users.noreply.github.com>
2018-03-05 23:22:05 -0600

    concerts to sql

--- videosearch.py
+++ videosearch.py
@@ -4,10 +4,15 @@ import pandas as pd
 
 
 # Use this filename for the database
-animee_df = pd.read_csv('Data/BLDataAnimee.txt', sep = '|', header = 0)
-conn = sqlite3.connect('animee.db')
-pd.animee_df.to_sql(name = 'animee_info.sqlite3', flavor = 'sqlite')
+DF_LIST = ['Animee', 'DailyLife', 'Dance', 'Domestic', 'Entertainment',
+           'Fashion', 'Games', 'Kichiku', 'Movies', 'Music', 'Science' ]
+CONN = sqlite3.connect('BLData.db')
+for cat in DF_LIST:
+
+    cat_df = pd.read_csv('Data/BLData{}.txt'.format(cat), sep = '|', header = 0)
+
+    cat_df.to_sql(name = cat, con = CONN, flavor = 'sqlite')
 
 DATA_DIR = os.path.dirname(__file__)
-DATABASE_FILENAME = os.path.join(DATA_DIR, 'animee_info.sqlite3')
+DATABASE_FILENAME = os.path.join(DATA_DIR, 'BLData.db')
 
